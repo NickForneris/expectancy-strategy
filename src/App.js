94ec4f9fe0e74d4c98d37a4a6e88fac541369beb
Nick Forneris <nicholasdforneris@gmail.com>
2022-12-15 01:24:16 -0600

    fix homepage error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from 'react';
+import {
+  Navbar,
+  Container,
+  Table,
+  Col,
+  Row
+} from 'react-bootstrap';
+
 
 function App() {
+
+  const [botData, setBotData] = useState()
+  const url = process.env.PUBLIC_URL + '/botdata/bots.json'
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) =>
+        setBotData(data)
+      )
+  }, [url])
+
+  const dataBody = () => (botData || []).map((bot, i) => {
+    return (
+      <tr key={i}>
+        <td className='align-middle'>{bot.name}</td>
+        <td className='align-middle'>{bot.pcount}</td>
+        <td className='align-middle'>${bot.seed}</td>
+        <td className='align-middle'>{bot.draw}</td>
+        <td className='align-middle'>${bot.openPnl}<br></br>{Math.round(bot.roi * 100)}%</td>
+      </tr>
+    )
+  })
+
+  const dataFoot = () => {
+    return (
+      <tr>
+        <td className='align-middle'>TOTALS</td>
+        <td className='align-middle'>{Object.values(botData || []).reduce((t, { pcount }) => t + pcount, 0)}</td>
+        <td className='align-middle'>${Object.values(botData || []).reduce((t, { seed }) => t + seed, 0)}</td>
+        <td className='align-middle'>${Object.values(botData || []).reduce((t, { draw }) => t + draw, 0)}</td>
+        <td className='align-middle'>${Object.values(botData || []).reduce((t, { openPnl }) => t + openPnl, 0)}
+        <br></br>{Object.values(botData || []).reduce((t, { roi }) => t + roi, 0) * 100}%
+        </td>
+      </tr>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar bg="dark" sticky="top">
+        <Container>
+          <Navbar.Brand className="text-light">Forneris: Expectancy Strategy Performance</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container className="pt-1">
+        <Row className="bg-dark d-flex aligns-items-center justify-content-center">
+          <Col>
+            <Table responsive className="text-light border">
+              <thead>
+                <tr>
+                  <th>BOT</th>
+                  <th>POSITIONS</th>
+                  <th>ALLOCATION</th>
+                  <th>CAP AT RISK</th>
+                  <th>P/L</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dataBody()}
+              </tbody>
+              <tfoot>
+                {dataFoot()}
+              </tfoot>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
 }
 
 export default App;
