094cb41ccb6172d9032811cf40cf47f15b373916
Nick Forneris <nicholasdforneris@gmail.com>
2023-02-01 15:43:31 -0600

    add trade ideas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import TradeIdeas from './TradeIdeas';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <TradeIdeas />
   </React.StrictMode>
 );
